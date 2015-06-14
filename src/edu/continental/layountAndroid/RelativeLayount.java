ccfdabe9e01d0f9ffe8032f5868d3e2535524b43
unknown <labb@WB02-2.continental.edu.pe>
2015-06-14 12:31:43 -0500

    procesando datos

---
+++ src/edu/continental/layountAndroid/RelativeLayount.java
@@ -0,0 +1,22 @@
+package edu.continental.layountAndroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RelativeLayount extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_relativ_layount);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.relativ_layount, menu);
+		return true;
+	}
+
+}
